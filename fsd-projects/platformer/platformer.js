8a1e1771afe9ecf98c834b5385aa241690d3d9b3
cedotal73329 <cedotal73329@students.apsb.org>
2026-08-26 19:07:21 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,11 +27,40 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
-
+     toggleGrid();
+  
     // TODO 2 - Create Platforms
-
+ createPlatform(10, 700, 100, 10, "darkblue");
+    createPlatform(400, 700, 5, 5, "darkblue");
+    createPlatform(490, 570, 4, 4, "darkblue");
+    createPlatform(200, 450, 100, 10, "darkblue");
+    createPlatform(100, 320, 30, 50, "darkblue");
+    createPlatform(390, 220, 10, 10, "darkblue");
+    createPlatform(700, 499, 2, 2, "red");
+    createPlatform(1000, 500, -100, -10, "darkblue");
+    createPlatform(600, 0, 50, 400, "darkblue");
+    createPlatform(500, 300, 1, 440, "darkblue");
+    createPlatform(580, 500, 120, 300, "darkblue");
+    createPlatform(700, 400, -50, -400, "darkblue");
+    createPlatform(0, 739, 1400, 1,);
+    createPlatform(600, 390, 100, 3, "white");
+    createPlatform(600, 280, 100, 3, "white");
+    createPlatform(600, 140, 100, 1, "darkblue");
+    createPlatform(900, 320, 100, 10, "darkblue");
+    createPlatform(800, 600, 100, 10, "lightblue");
+    createPlatform(1250, 700, 10, 10, "lightblue");
+    createCollectable("database", 930, 250, 0, 1);
+    createCollectable("database", 660, 50, 0, 1);
+    createCollectable("database", 1250,650, 0, 1);
+    createPlatform(1200, 100, 1, 380, "darkblue");
+    createPlatform(1350, 590, 10, 10, "darkblue");
+    createPlatform(1250, 480, 10, 10, "darkblue");
+    createPlatform(1350, 360, 10, 10, "darkblue");
+    createPlatform(100, 0, 1, 550, "darkblue");
+    createPlatform(1250, 240, 10, 10, "darkblue");
+    createPlatform(1350, 190, 10, 10, "darkblue");
+    createPlatform(1100, 300, 3, 3, "darkblue");
+    createCannon( "top", 1360, 1500);
 
 
 
@@ -41,7 +70,8 @@ $(function () {
 
     
     // TODO 4 - Create Cannons
-
+    createCannon("top", 650, 1300)
+    createCannon("top", 970, 900)
 
     
     
